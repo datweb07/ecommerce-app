982b844c61215b377995c069c72efedda5e88eb5
datweb07 <dat82770@gmail.com>
2025-12-20 22:42:06 +0700

    initial all source

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
