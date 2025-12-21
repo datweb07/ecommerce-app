36f02f6b3ce78f7775e4d712c02fdf3ce3fb1baf
datweb07 <dat82770@gmail.com>
2025-12-21 11:33:14 +0700

    authentication implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,17 @@ import express from "express";
 import path from "path";
 
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inggest.js";
 import { ENV } from "./config/env.js";
-import { connect } from "http2";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-app.use(clerkMiddleware());
-
 const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions: functions }));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
