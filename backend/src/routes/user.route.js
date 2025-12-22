a77dc8ec27f1cb8fe1ae0f4d5924a3033b7e803a
datweb07 <dat82770@gmail.com>
2025-12-22 21:52:59 +0700

    added: user route & controller

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  addAddress,
+  getAddress,
+  updateAddress,
+  deleteAddress,
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist,
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.use(protectRoute);
+
+router.post("/addresses", addAddress);
+router.get("/addresses", getAddress);
+router.put("/addresses/:addressId", updateAddress);
+router.put("/addresses/:addressId", deleteAddress);
+
+router.post("/wishlist", addToWishlist);
+router.delete("/wishlist/:productId", removeFromWishlist);
+router.get("/wishlist", getWishlist);
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { functions, inngest } from "./config/inggest.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -14,16 +15,18 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions: functions }));
+
 app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
+// ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  // ready for deployment
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
