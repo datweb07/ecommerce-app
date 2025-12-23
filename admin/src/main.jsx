947dc9244e70a89b160a58c522b4a14e793abd50
datweb07 <dat82770@gmail.com>
2025-12-24 00:29:10 +0700

    feat: implement product management features including CRUD operations and integrate Sentry for error tracking

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
+import * as Sentry from "@sentry/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,6 +15,13 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
